a690f0f21524d6e9e3b32043741a03678e2b8a2a
Vaspero <jd1998@o2.pl>
2017-09-13 10:32:48 +0200

    ... v2

---
+++ js/new.js
@@ -0,0 +1,19 @@
+/*function start(){
+	document.write("witaj, " + name);
+}
+
+var name = "Janus";
+start(name);
+
+function kwa(){
+	document.write(prompt("a") * prompt("b"));
+}
+kwa();*/
+//document.getElementById("butt").onclick = ok();
+
+function ok(){
+	var res = document.getElementById("wyn");
+	var a = document.getElementById("a").value;
+	var b = document.getElementById("b").value);
+	res.innerHTML(a * b);
+}
