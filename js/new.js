c49d0e2639a5d264fb6daa99abbd01c9e6bf72e6
Vaspero <jd1998@o2.pl>
2017-09-26 09:38:46 +0200

    xd lol

--- js/new.js
+++ js/new.js
@@ -1,19 +1,58 @@
-/*function start(){
-	document.write("witaj, " + name);
+/*for(var i = 0; i < 101; i++){
+	if(i % 15 == 0){
+		document.body.innerHTML +="FizzBuzz";
+	} else if( i % 3 == 0){
+		document.body.innerHTML +="Fizz";
+	} else if( i % 5 == 0){
+		document.body.innerHTML +="Buzz";
+	} else document.body.innerHTML += i;
+	document.innerHTML += "<br>"
+}*/
+
+/*var wut = document.getElementsByName("a");
+var btn = document.getElementById("btn");
+var b = true;
+btn.onclick = function(){
+	console.log(wut);
+	for(var i = 0; i < wut.length; i++){
+		wut[i].className = 'wut';
+	}
+}
+btn.onclick = function(){
+	var input = document.getElementById('input');
+	input.setAttribute('type', 'button');
+	input.onclick = function(){input.setAttribute('value', 'Kot zdycha na dachu');}
+	wut.forEach(zmien);
+	if (b) b=false;
+	else b = true;
 }
 
-var name = "Janus";
-start(name);
-
-function kwa(){
-	document.write(prompt("a") * prompt("b"));
+function zmien(element){
+	if(b){element.className = 'wut'; b = false;}
+	else {element.className = 'wut2'; b=true;}
 }
-kwa();*/
-//document.getElementById("butt").onclick = ok();
 
-function ok(){
-	var res = document.getElementById("wyn");
-	var a = document.getElementById("a").value;
-	var b = document.getElementById("b").value);
-	res.innerHTML(a * b);
+var lista = document.getElementsByTagName('ol')[0];
+var first = lista.firstElementChild;
+first.setAttribute('class', 'wut3');*/
+
+var div = document.getElementById("block");
+var namea = document.getElementById("name");
+var surname = document.getElementById("surname");
+var login = document.getElementById("login");
+var email = document.getElementById("email");
+var email2 = document.getElementById("email2");
+var pass = document.getElementById("pass");
+var pass2 = document.getElementById("pass2");
+var box = document.getElementById("box");
+
+/*window.onload = function(){
+	alert("nie powinieneś żyć");
+}*/
+
+namea.addEventListener("blur", sth);
+
+function sth(){
+	console.log("ss");
+	email.disabled = true;
 }
