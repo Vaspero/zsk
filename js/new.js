2bde826f21a67720ded0aae3d31901c7d11e4545
Vaspero <jd1998@o2.pl>
2017-09-27 09:23:45 +0200

    whatever

--- js/new.js
+++ js/new.js
@@ -37,6 +37,7 @@ var first = lista.firstElementChild;
 first.setAttribute('class', 'wut3');*/
 
 var div = document.getElementById("block");
+var result = document.getElementById("result");
 var namea = document.getElementById("name");
 var surname = document.getElementById("surname");
 var login = document.getElementById("login");
@@ -45,14 +46,77 @@ var email2 = document.getElementById("email2");
 var pass = document.getElementById("pass");
 var pass2 = document.getElementById("pass2");
 var box = document.getElementById("box");
+var date = document.getElementById("date");
+var btn = document.getElementById("btn");
+var btn2 = document.getElementById("btn2");
+var fieldset = document.getElementById("fieldset");
+var filled = 0;
 
-/*window.onload = function(){
-	alert("nie powinieneś żyć");
-}*/
-
-namea.addEventListener("blur", sth);
-
-function sth(){
-	console.log("ss");
-	email.disabled = true;
+namea.onblur = function(){
+	if(namea.value.length < 2) div.textContent = "wpisz poprawne imie";
+	else{
+		namea.disabled = true;
+		div.textContent="";
+		filled++;
+	}
 }
+surname.onblur = function(){
+	if(surname.value.length < 2) div.textContent = "wpisz poprawne nazwisko";
+	else{
+		surname.disabled = true;
+		div.textContent="";
+		filled++;
+	}
+}
+login.onblur = function(){
+	if(login.value.length < 2) div.textContent = "wpisz poprawny login";
+	else{
+		login.disabled = true;
+		div.textContent="";
+		filled++;
+	}
+}
+email.onblur = function(){
+	if(email.value.length < 2) div.textContent = "wpisz poprawny email";
+	else{
+		email.disabled = true;
+		div.textContent="";
+		filled++;
+	}
+}
+pass.onblur = function(){
+	if(pass.value.length < 5) div.textContent = "wpisz poprawne hasło";
+	else{
+		pass.disabled = true;
+		div.textContent="";
+		filled++;
+	}
+}
+box.onchange = function(){
+	if(box.checked) btn.disabled = false;
+	else btn.disabled = true;
+}
+btn2.onclick = function(){
+	var block = document.getElementsByTagName('input');
+	for(var i = 0; i < block.length; i++ ){
+		if(block[i].disabled == true){
+			block[i].disabled = false;
+			block[i].value = "";
+			filled--;
+		}
+	}
+}
+btn.onclick = function(){
+	console.log(filled);
+	if(filled != 5){
+		div.textContent = "wypełnij wszystkie pola";
+	}else{
+		fieldset.style.display = "none";
+		result.innerHTML += "imie: " +namea.value + "<br>";
+		result.innerHTML += "nazwisko: " +surname.value + "<br>";
+		result.innerHTML += "login: " +login.value + "<br>";
+		result.innerHTML += "email: " +email.value + "<br>";
+		result.innerHTML += "hasło: " +pass.value;
+	}
+}
+
